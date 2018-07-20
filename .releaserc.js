c8526f6cdb8cad880bb621a3b1870714a2c63b44
Anton Golub <mailbox@antongolub.ru>
2018-07-20 17:47:05 +0300

    build: add `release-hooks`

---
+++ .releaserc.js
@@ -0,0 +1,22 @@
+const hooks = require('semantic-release-monorepo-hooks')
+const output = hooks()
+const publish = output.isLastChanged
+  ? [
+    '@semantic-release/github',
+    '@semantic-release/npm'
+  ]
+  : [
+    '@semantic-release/npm'
+  ]
+
+module.exports = {
+  branch: 'master',
+  tagFormat: 'v${version}',
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  publish: publish,
+  verifyConditions: ['@semantic-release/npm', '@semantic-release/git']
+};
